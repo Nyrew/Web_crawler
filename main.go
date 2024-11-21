059f9e6209d25881e1d0899e8b8f7e8dc2ddbd05
Nyrew <lukas.vokel@gmail.com>
2024-11-21 14:25:40 +0100

    Get HTML

--- main.go
+++ main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	if len(os.Args) < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	}
+	if len(os.Args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+	rawBaseURL := os.Args[1]
+
+	fmt.Printf("starting crawl of: %s...\n", rawBaseURL)
+
+	html, err := getHTML(rawBaseURL)
+	if err != nil {
+		fmt.Printf("error fetching HTML: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Fetched HTML:\n", html)
 }
